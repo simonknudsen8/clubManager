03c3d5ac830f1cd1a3daf8da6acff4ab71c11837
simonknudsen8 <simon.knudsen78@gmail.com>
2025-06-11 14:23:28 +0200

    update some things

--- src/main/java/com/simon/clubmanager/controller/UserController.java
+++ src/main/java/com/simon/clubmanager/controller/UserController.java
@@ -3,21 +3,37 @@ package com.simon.clubmanager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.simon.clubmanager.models.User;
 
 @Controller
 @RequestMapping("")
 public class UserController {
-
+    
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/")
     public String mainPage(){
         return "mainPage";
     }
 
-
     @GetMapping("/login")
-    public String loginPage(){
-        return "loginPage";
+    public String loginPage(Model model){
+        logger.info("Loading login page");
+        model.addAttribute("user", new User());
+        return "logInPage";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user){
+        logger.info("Login attempt for user: {}", user.getMail());
+        // Add your login logic here
+        return "mainPage";  // Redirect back to main page after login
     }
 }
--- src/main/java/com/simon/clubmanager/models/User.java
+++ src/main/java/com/simon/clubmanager/models/User.java
@@ -10,11 +10,55 @@ public class User {
     private LocalDate birthday;
     private Grade grade;
 
+    public User() {
+        // No-args constructor
+    }
+
     public User(String mail, String name, String password, LocalDate birthday, Grade grade) {
         this.mail = mail;
         this.name = name;
         this.password = password;
         this.birthday = birthday;
+        this.grade = grade;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
         this.grade = grade;
     }
 }
